08c86be0dc59d80f76b7fef9d1661c8a97e2a62d
Teinelle Charles <teinelledcharles@gmail.com>
2020-10-17 09:46:43 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,40 @@
+class TicTacToe
+ 
+  def initialize
+    @board = Array.new (9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2],]
+    
+    def display_board
+    puts" #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "---------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "---------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+end
+  
+
+    def input_to_index(index)
+    @choose = string.to_i - 1
+    end
+  
+    def move(input_to_index,player)
+    @board[input_to_index] = player
+    end
+
+    def position_taken(input_to_index)
+    if @board[input_to_index] == 'X' || 'O'
+    end
+  end
+
+end
--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -1,147 +1,133 @@
-require_relative '../lib/tic_tac_toe.rb'
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
-describe './lib/tic_tac_toe.rb' do
-  describe TicTacToe do
-    describe '#play' do
-      it 'asks for players input on a turn of the game' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:over?).and_return(false, true)
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-        expect(game).to receive(:gets).at_least(:once).and_return("1")
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-        game.play
-      end
+def move(board, index, current_player)
+  board[index] = current_player
+end
 
-      it 'checks if the game is over after every turn' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:gets).and_return("1", "2", "3")
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
-        expect(game).to receive(:over?).at_least(:twice).and_return(false, false, true)
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
-        game.play
-      end
-
-      it 'plays the first turn of the game' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:gets).and_return("1")
-
-        allow(game).to receive(:over?).and_return(false, true)
-
-        game.play
-        board_after_first_turn = game.instance_variable_get(:@board)
-        expect(board_after_first_turn).to match_array(["X", " ", " ", " ", " ", " ", " ", " ", " "])
-      end
-
-      it 'plays the first few turns of the game' do
-        game = TicTacToe.new
-
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:gets).and_return("1","2","3")
-        allow(game).to receive(:over?).and_return(false, false, false, true)
-
-        game.play
-
-        board_after_three_turns = game.instance_variable_get(:@board)
-        expect(board_after_three_turns).to match_array(["X", "O", "X", " ", " ", " ", " ", " ", " "])
-      end
-
-      it 'checks if the game is won after every turn' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:gets).and_return("1", "2", "3")
-        allow(game).to receive(:winner).and_return("X")
-
-        expect(game).to receive(:won?).at_least(:twice).and_return(false, false, true)
-
-        game.play
-      end
-
-      it 'checks if the game is draw after every turn' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-        allow(game).to receive(:gets).and_return("1", "2", "3")
-
-        expect(game).to receive(:draw?).at_least(:twice).and_return(false, false, true)
-
-        game.play
-      end
-
-      it 'stops playing if someone has won' do
-        game = TicTacToe.new
-        board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-        game.instance_variable_set(:@board, board)
-
-        allow($stdout).to receive(:puts)
-
-        expect(game).to_not receive(:turn)
-
-        game.play
-      end
-
-      it 'congratulates the winner X' do
-        game = TicTacToe.new
-        board = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
-        game.instance_variable_set(:@board, board)
-        allow($stdout).to receive(:puts)
-
-        expect($stdout).to receive(:puts).with("Congratulations X!")
-
-        game.play
-      end
-
-      it 'congratulates the winner O' do
-        game = TicTacToe.new
-        board = ["X", "X", " ", "X", " ", " ", "O", "O", "O"]
-        game.instance_variable_set(:@board, board)
-
-        allow($stdout).to receive(:puts)
-
-        expect($stdout).to receive(:puts).with("Congratulations O!")
-
-        game.play
-      end
-
-      it 'stops playing in a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
-        allow($stdout).to receive(:puts)
-
-        expect(game).to_not receive(:turn)
-
-        game.play
-      end
-
-      it 'prints "Cat\'s Game!" on a draw' do
-        game = TicTacToe.new
-        board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        game.instance_variable_set(:@board, board)
-        allow($stdout).to receive(:puts)
-
-        expect($stdout).to receive(:puts).with("Cat's Game!")
-
-        game.play
-      end
-
-      it 'plays through an entire game' do
-        game = TicTacToe.new
-        allow($stdout).to receive(:puts)
-
-        expect(game).to receive(:gets).and_return("1")
-        expect(game).to receive(:gets).and_return("2")
-        expect(game).to receive(:gets).and_return("3")
-        expect(game).to receive(:gets).and_return("4")
-        expect(game).to receive(:gets).and_return("5")
-        expect(game).to receive(:gets).and_return("6")
-        expect(game).to receive(:gets).and_return("7")
-
-        expect($stdout).to receive(:puts).with("Congratulations X!")
-
-        game.play
-      end
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
     end
+  end
+  return turn
+end
+
+def current_player(board)
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count(board)
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+
+def turn(board)
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_token = current_player(board)
+    move(board, index, player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+def play(board)
+  until over?(board) == true
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
   end
 end
